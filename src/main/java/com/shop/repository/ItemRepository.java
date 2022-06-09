82b746ebc2c8361c6edbfd381e09b7f3d9d809ce
강재원 <gangjaewon@gangjaewon-ui-MacBookAir.local>
2022-06-09 09:47:40 +0900

    Initial commit

---
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -0,0 +1,14 @@
+package com.shop.repository;
+
+import com.shop.entity.Item;
+import com.shop.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item>, ItemRepositoryCustom {
+
+    List<Item> findByItemNm(String itemNm);
+
+}
